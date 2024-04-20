9cb5039aefd95c6e313b1bd32eab3dad7de063fa
Ankit1927 <67674233+Ankit1927@users.noreply.github.com>
2024-04-20 17:07:32 +0530

    Add files via upload
    First run remark_add.py script after that execute this script

---
+++ second_code.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def add_remark(df):
+    for index, row in df.iterrows():
+        if "UPI" in row['Narration'] and pd.notnull(row['Deposit Amt.']) <= 999 :
+            if pd.isnull(row['REMARK']):
+                df.at[index, 'REMARK'] = "PERSONAL USE ONLINE CREDIT"
+    return df
+
+def main():
+    # Prompt user for file path
+    file_path = input("Enter the file path: ")
+
+    # Read Excel file
+    df = pd.read_excel(file_path)
+
+    # Apply the operation
+    df = add_remark(df)
+
+    # Save output to a new Excel file
+    output_file_path = "output.xlsx"
+    df.to_excel(output_file_path, index=False)
+    print("Output saved to", output_file_path)
+
+if __name__ == "__main__":
+    main()
